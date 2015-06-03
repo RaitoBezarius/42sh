83e83e3a49b7212cc8c0a94c787700dd7907fd0f
Raito Bezarius <masterancpp@gmail.com>
2015-06-03 15:12:53 +0200

    Add push function for the linked list structure.

--- include/utils/linked_list.h
+++ include/utils/linked_list.h
@@ -14,6 +14,7 @@ t_linked_list	*create_linked_list();
 void	free_linked_list(t_linked_list	*list);
 
 /** Linked list manipulation functions **/
+void	push_to_linked_list(t_linked_list	*current, void	*item, int	type);
 void	remove_linked_item(t_linked_list	*item);
 void	move_item_before(t_linked_list	*from, t_linked_list	*to);
 
