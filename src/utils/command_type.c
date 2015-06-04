6b4cf6cd3dd84ebc009616fde6ab61e76f842c73
Raito Bezarius <masterancpp@gmail.com>
2015-06-04 21:31:38 +0200

    is_builtin && is_alias implementations.

--- src/utils/command_type.c
+++ src/utils/command_type.c
@@ -2,14 +2,30 @@
 
 #include "definitions.h"
 
-/** TODO: Code them. **/
-
 int	is_alias(char	*executable)
 {
+	t_alias	*current;
+
+	current = g_alias_list;
+	while (current)
+	{
+		if (strcmp(current->name, executable) == 0)
+			return TRUE;
+		current = current->next;
+	}
 	return FALSE;
 }
 
 int is_builtin(char	*executable)
 {
+	int index;
+	
+	index = 0;
+	while (g_builtins[index].name != NULL)
+	{
+		if (g_builtins[index].name == executable)
+			return TRUE;
+		index++;
+	}
 	return FALSE;
 }
