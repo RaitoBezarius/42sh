02011843bac23241191ed364f0bbaf37ccb8695b
BEBE-BELL Hendy-Wilson <bebe-b_h@epitech.eu>
2015-06-04 13:39:34 +0200

    Correction de strcut. (et mise à la norme)

--- src/utils/strcut.c
+++ src/utils/strcut.c
@@ -5,7 +5,7 @@
 ** Login   <bebe-b_h@epitech.eu>
 **
 ** Started on  Fri May 22 16:33:28 2015 BEBE-BELL Hendy-Wilson
-** Last update Fri May 22 16:33:52 2015 BEBE-BELL Hendy-Wilson
+** Last update Thu Jun 04 13:39:15 2015 BEBE-BELL Hendy-Wilson
 */
 
 #include <stdlib.h>
@@ -15,61 +15,55 @@
 
 /*
 ** Fonction sur laquelle repose tout le programme
-** DO NOT DELETE ! NE PAS SUPPRIMER ! LA MEME DANS UNE AUTRE LANGUE !
+** DO NOT DELETE ! NE PAS SUPPRIMER ! PAREIL DANS UNE AUTRE LANGUE !
 */
 int is_blank(char c)
 {
-	return c == ' ';
+  return (c == ' ' || c == '\t');
 }
 
-t_strip_location count_stripped(const char	*str, const int s_idx, const int e_idx)
+static t_strip_location count_stripped(const char *str, const int s_idx,
+				       const int e_idx)
 {
-	t_strip_location location;
+  t_strip_location location;
 
-	location.start_index = s_idx;
-	while (location.start_index < e_idx && is_blank(str[location.start_index]))
-		++location.start_index;
-	location.end_index = e_idx;
-	while (location.end_index > s_idx && is_blank(str[location.end_index]))
-		--location.end_index;
-	return location;
+  location.start_index = s_idx;
+  while (location.start_index < e_idx && is_blank(str[location.start_index]))
+    ++location.start_index;
+  location.end_index = e_idx;
+  while (location.end_index > s_idx && is_blank(str[location.end_index]))
+    --location.end_index;
+  return (location);
 }
 
 /** TODO CRITICAL: strcut(line, 0, 1) will always return NULL
-**/
-
-char	*strcut(const char *str, const int s_idx, const int e_idx)
+ **/
+char			*strcut(const char *str, const int s_idx,
+				const int e_idx)
 {
-  char	*tmp;
-  int	location;
-	int index;
-	t_strip_location s_location;
+  char			*tmp;
+  int			index;
+  t_strip_location	s_location;
 
-  if (str == NULL || s_idx >= e_idx)
+  if (str == NULL || s_idx > e_idx)
     return (NULL);
-	
-	s_location = count_stripped(str, s_idx, e_idx);
-
-	if ((s_location.end_index - s_location.start_index) <= 0)
-		return (NULL);
-
-  tmp = malloc(sizeof(char) * (s_location.end_index - s_location.start_index) + 2);
+  s_location = count_stripped(str, s_idx, e_idx);
+  if ((s_location.end_index - s_location.start_index) < 0)
+    return (NULL);
+  tmp = malloc(sizeof(char) * (s_location.end_index - s_location.start_index
+			       + 2));
   if (tmp == NULL)
   {
     fprintf(stderr, "Error malloc\n");
     return (NULL);
   }
-
-  location = s_location.start_index;
-	index = 0;
-
-	while (location <= s_location.end_index)
-	{
-		tmp[index] = str[location];
-		location++;
-		index++;
-	}
-
+  index = 0;
+  while (s_location.start_index <= s_location.end_index)
+  {
+    tmp[index] = str[s_location.start_index];
+    ++s_location.start_index;
+    ++index;
+  }
   tmp[index] = '\0';
   return (tmp);
 }
